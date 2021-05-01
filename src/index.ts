8266862ec5976f442233235492467ecb5614dcd3
arpitnath <arpitnath42@gmail.com>
2021-05-01 18:34:47 +0530

    micro orm setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello there! TEST')
+import { MikroORM } from '@mikro-orm/core'
+import microConfig from './mikro-orm.config'
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig)
+  await orm.getMigrator().up()
+
+  //   const post = orm.em.create(Post, { title: 'my first post' })
+  //   await orm.em.persistAndFlush(post)
+
+  //   const posts = await orm.em.find(Post, {})
+  //   console.log(posts)
+}
+
+main().catch((err) => {
+  console.error(err)
+})
