7ee2ce92d6c1b8ce487ed8bb4bd3772f70e89625
arpitnath <arpitnath42@gmail.com>
2021-05-01 20:01:37 +0530

    graphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import { MikroORM } from '@mikro-orm/core'
 import microConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig)
   await orm.getMigrator().up()
 
-  //   const post = orm.em.create(Post, { title: 'my first post' })
-  //   await orm.em.persistAndFlush(post)
+  const app = express()
 
-  //   const posts = await orm.em.find(Post, {})
-  //   console.log(posts)
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  })
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000')
+  })
 }
 
 main().catch((err) => {
